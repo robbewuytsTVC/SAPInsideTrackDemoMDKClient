312cba1530b4865746d0b39a16bd6576565e17aa
Robbe Wuyts <robbe.wuyts@thevaluechain.be>
2023-09-17 14:35:54 +0200

    loading screen

--- MDKDemo.mdkproject/App_Resources/iOS/src/ContentView.swift
+++ MDKDemo.mdkproject/App_Resources/iOS/src/ContentView.swift
@@ -12,99 +12,129 @@ struct ContentView: View {
     @State private var gameOver = false
     @State private var level: Int = 1
     @State private var opacity: Double = 1.0
+    @State private var showLoadingScreen: Bool = true
     
     var body: some View {
-        ZStack {
-            //Frame
-            Rectangle()
-                .fill(Color(red: 139 / 255, green: 172 / 255, blue: 15 / 255))
-                .frame(maxWidth: 350, maxHeight: 650)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(.black, lineWidth: 5)
-                )
-            
-            //Title
-            Text("SAP Inside Track goes 8bit")
-                .font(.custom("Early GameBoy", size: 20))
-                .multilineTextAlignment(.center)
-                .foregroundColor(Color.black)
-                .offset(y: -250)
-            
-            //Ball
-            Rectangle()
-                .fill(Color.black)
-                .frame(width: 25, height: 25)
-                .position(ballPosition)
-                .opacity(gameOver ? 0 : 1)
-            
-            //Paddle
-            Rectangle()
-                .fill(Color.black)
-                .frame(width: 100, height: 10)
-                .position(x: paddlePosition, y: 625)
-                .opacity(gameOver ? 0 : 1)
-            
-            //Score
-            Text("Score: \(score)")
-                .foregroundColor(.black)
-                .font(.custom("Early GameBoy", size: 18))
-                .position(x: 90, y: 170)
-            
-            //Level
-            Text("Level: \(level)")
-                .foregroundColor(.black)
-                .font(.custom("Early GameBoy", size: 18))
-                .position(x: 90, y: 140)
-            
-            
-            if gameOver {
-                //Game over text
-                Text("GAME OVER!")
-                    .font(.custom("Early GameBoy", size: 30))
-                    .foregroundColor(Color.black)
+        
+            ZStack {
+                //Frame
+                Rectangle()
+                    .fill(Color(red: 139 / 255, green: 172 / 255, blue: 15 / 255))
+                    .frame(maxWidth: 350, maxHeight: 650)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.black, lineWidth: 5)
+                    )
                 
-                //TVC text
-                Text("©2023 TheValueChain")
-                    .foregroundColor(.black)
-                    .font(.custom("Early GameBoy", size: 15))
-                    .position(x: 170, y: 610)
-                
-                //Start text
-                Text("Press start to play again!")
-                    .font(.custom("Early GameBoy", size: 11))
-                    .foregroundColor(Color.black)
-                    .position(x: 180, y: 380)
-                    .opacity(opacity)
-                    .onAppear {
-                        startBlinking()
+                // Loading screen
+                if showLoadingScreen {
+                    VStack {
+                        Text("SAP Inside Track 2023")
+                            .font(.custom("Early GameBoy", size: 22))
+                            .foregroundColor(.black)
+                            .padding(.bottom, 20)
+                        
+                        Text("MDK PONG Game")
+                            .font(.custom("Early GameBoy", size: 26))
+                            .foregroundColor(.black)
+                        
+                        
+                        Image("SAP3")  // Replace with your image name
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 200, height: 200)
+                        
+                        Text("Loading...")
+                            .font(.custom("Early GameBoy", size: 20))
                     }
-                
-                //Start button
-                Button(action: {
-                    restartGame()
-                }) {
-                    Text("START")
+                } else {
+                    //Title
+                    Text("SAP Inside Track goes 8bit")
                         .font(.custom("Early GameBoy", size: 20))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.black)
+                        .offset(y: -250)
+                    
+                    //Ball
+                    Rectangle()
+                        .fill(Color.black)
+                        .frame(width: 25, height: 25)
+                        .position(ballPosition)
+                        .opacity(gameOver ? 0 : 1)
+                    
+                    //Paddle
+                    Rectangle()
+                        .fill(Color.black)
+                        .frame(width: 100, height: 10)
+                        .position(x: paddlePosition, y: 625)
+                        .opacity(gameOver ? 0 : 1)
+                    
+                    //Score
+                    Text("Score: \(score)")
                         .foregroundColor(.black)
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 10)
+                        .font(.custom("Early GameBoy", size: 18))
+                        .position(x: 90, y: 170)
+                    
+                    //Level
+                    Text("Level: \(level)")
+                        .foregroundColor(.black)
+                        .font(.custom("Early GameBoy", size: 18))
+                        .position(x: 90, y: 140)
+                    
+                    
+                    if gameOver {
+                        //Game over text
+                        Text("GAME OVER!")
+                            .font(.custom("Early GameBoy", size: 30))
+                            .foregroundColor(Color.black)
+                        
+                        //TVC text
+                        Text("©2023 TheValueChain")
+                            .foregroundColor(.black)
+                            .font(.custom("Early GameBoy", size: 15))
+                            .position(x: 170, y: 610)
+                        
+                        //Start text
+                        Text("Press start to play again!")
+                            .font(.custom("Early GameBoy", size: 11))
+                            .foregroundColor(Color.black)
+                            .position(x: 180, y: 380)
+                            .opacity(opacity)
+                            .onAppear {
+                                startBlinking()
+                            }
+                        
+                        //Start button
+                        Button(action: {
+                            restartGame()
+                        }) {
+                            Text("START")
+                                .font(.custom("Early GameBoy", size: 20))
+                                .foregroundColor(.black)
+                                .padding(.horizontal, 20)
+                                .padding(.vertical, 10)
+                        }
+                        .background(Color.white)
+                        .cornerRadius(5)
+                        .position(x: 170, y: 450)
+                    }
                 }
-                .background(Color.white)
-                .cornerRadius(5)
-                .position(x: 170, y: 450)
             }
-        }
-        .gesture(
-            DragGesture()
-                .onChanged { value in
-                    paddlePosition = value.location.x
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        paddlePosition = value.location.x
+                    }
+            )
+            .onAppear {
+                // Simulate loading for 3 seconds
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    self.showLoadingScreen = false
+                    startGame()
                 }
-        )
-        .onAppear {
-            startGame()
-        }
-        .frame(width: 350, height: 650)
+            }
+            .frame(width: 350, height: 650)
+        
     }
     
     func startGame() {
@@ -138,7 +168,7 @@ struct ContentView: View {
         ballPosition.x += dx
         ballPosition.y += dy
         
-        if ballPosition.x <= 15     || ballPosition.x >= 335 { // Frame is 350 wide, ball is 25 wide
+        if ballPosition.x <= 15	 || ballPosition.x >= 335 { // Frame is 350 wide, ball is 25 wide
             dx = -dx
         }
         
